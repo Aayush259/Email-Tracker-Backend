6c4b035140730115fb72cd1c90fd95c6bf48352a
Aayush Kumar Kumawat <159794430+Aayush259@users.noreply.github.com>
2024-10-25 01:40:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const connectToMongoDB = async () => {
 
   try {
     await mongoose.connect(
-      `mongodb+srv://${username}:${password}@cluster0.bbhnx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+      `mongodb+srv://${username}:${password}@cluster0.bbhnx.mongodb.net/ecommerce?retryWrites=true&w=majority`
     );
     console.log('Connected to MongoDB');
   } catch (error) {
